5c3ca0952d5638d911f8d9bfa8197c7643c6085e
Nic <nicdevel@gmail.com>
2013-02-28 22:56:20 -0500

    symbolic differentiation and also implementation of ex. 2.56 to differentiate exponentiation

--- Chapter_2.rkt
+++ Chapter_2.rkt
@@ -377,3 +377,94 @@
          ((and (null? x)(null? y)) #t)
             ((eq? (car x)(car y))(my-equal? (cdr x)(cdr y)))
             (else #f)))
+
+;symbolic differentiation
+(define (variable? e)
+  (symbol? e))
+
+(define (same-variable? v1 v2)
+  (and (variable? v1)(variable? v2)(eq? v1 v2)))
+
+(define (=number? exp num)
+  (and (number? exp)(= exp num)))
+
+(define (make-sum a1 a2)
+  (cond ((=number? a1 0) a2)
+        ((=number? a2 0) a1)
+        ((and (number? a1)(number? a2)(+ a1 a2)))
+        (else (list '+ a1 a2))))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0)(=number? m2 0)) 0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1)(number? m2))(* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (sum? expr)
+  (and (pair? expr)(eq? '+ (car expr))))
+
+(define (addend s)
+  (cadr s))
+
+(define (augend s)
+  (caddr s))
+
+(define (product? x)
+  (and (pair? x)(eq? '* (car x))))
+
+(define (multiplier p)
+  (cadr p))
+
+(define (multiplicand p)
+  (caddr p))
+
+;Ex. 2.56
+;;; extend (deriv...) to be able to deferentiate exponention 
+;;; expressions
+
+;;; helper function
+;;; I will define n^{x} as (^ n x)
+
+(define (exponentiation? expr)
+  (and (pair? expr)(eq? '^ (car expr))))
+
+(define (make-exponentiation b e)
+  (cond ((or (=number? e 0)(=number? b 1)) 1)
+        ((=number? b 0) 0)
+        ((=number? e 1) b)
+        ((and (number? b)(number? e))(expt b e))
+        (else (list '^ b e))))
+
+(define (base expr)
+  (cadr expr))
+
+(define (exponent expr)
+  (caddr expr))
+
+; derivative 
+(define (deriv expr var)
+  "Derivative of expr with respect to var"
+  (cond ((number? expr) 0)
+        ((variable? expr)
+         (if (same-variable? expr var) 1 0))
+        ((sum? expr)
+         (make-sum (deriv (addend expr) var)
+                   (deriv (augend expr) var)))
+        ((product? expr)
+         (make-sum 
+          (make-product (multiplier expr)
+                        (deriv (multiplicand expr) var))
+          (make-product (multiplicand expr)
+                        (deriv (multiplier expr) var))))
+        ;;Ex. 2.56 work
+        ((exponentiation? expr)
+         (make-product
+          (make-product (exponent expr)
+                        (make-exponentiation (base expr)
+                                             (make-sum (exponent expr)
+                                                       -1)))
+          (deriv (base expr) var)))
+        (else 
+         (error "unknown expression type -- DERIV" expr))))
+
