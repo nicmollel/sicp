2995be275d6cccb78adb500a8a68b2c27a1f9f6c
Nic <nicdevel@gmail.com>
2013-03-04 22:08:49 -0500

    Sets as unordered lists

--- Chapter_2.rkt
+++ Chapter_2.rkt
@@ -498,3 +498,30 @@
         (else 
          (error "unknown expression type -- DERIV" expr))))
 
+
+; 2.3.3 Representing Sets
+
+;Sets as unordered lists
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((equal? (car set) x) #t)
+        (else (element-of-set? x (cdr set)))))
+
+(define (adjoin-set x set)
+  (if (element-of-set? x set)
+      set 
+      (cons x set)))
+
+(define (intersection-set set1 set2)
+  (cond ((or (null? set1)(null? set2)) '())
+        ((element-of-set? (car set1) set2)
+         (cons (car set1)
+               (intersection-set (cdr set1) set2)))
+        (else (intersection-set (cdr set1) set2))))
+
+;Ex. 2.59
+(define (union-set set1 set2)
+  (if (null? set1)
+      set2
+      (adjoin-set (car set1)(union-set (cdr set1) set2))))
