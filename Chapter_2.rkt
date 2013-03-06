79bb75ef2f296ee6456c3cd7372e5cf467aff399
Nic <nicdevel@gmail.com>
2013-03-05 22:17:15 -0500

    Sets as ordered lists and related exercises

--- Chapter_2.rkt
+++ Chapter_2.rkt
@@ -501,27 +501,49 @@
 
 ; 2.3.3 Representing Sets
 
-;Sets as unordered lists
+;Sets as ordered lists...items are assumed to be numbers
 
 (define (element-of-set? x set)
   (cond ((null? set) #f)
-        ((equal? (car set) x) #t)
+        ((= x (car set) #t))
+        ((< x (car set) #f))
         (else (element-of-set? x (cdr set)))))
 
+;Ex. 2.61
 (define (adjoin-set x set)
-  (if (element-of-set? x set)
-      set 
-      (cons x set)))
+  (cond ((or (null? set)(< x (car set)))(cons x set))
+        ((= x (car set)) set)
+        ;; greater than the first element 
+        (else (cons (car set) 
+                    (adjoin-set x (cdr set))))))
 
 (define (intersection-set set1 set2)
-  (cond ((or (null? set1)(null? set2)) '())
-        ((element-of-set? (car set1) set2)
-         (cons (car set1)
-               (intersection-set (cdr set1) set2)))
-        (else (intersection-set (cdr set1) set2))))
+  (if (or (null? set1)(null? set2))
+      '()
+      (let ((x1 (car set1))
+            (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1 (intersection-set (cdr set1) (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
 
-;Ex. 2.59
+;Ex. 2.59, 2.62
+; 2.62, my initial solution was based on using adjoin-set and this 
+; would not meet the O(n) time requirement. This solution is 
+; a modification of that that and borrowed ideas from schemewiki
+
+; not that ordered list assume unique elements without repitition. 
+; repetition will break the union-set defined below. 
 (define (union-set set1 set2)
-  (if (null? set1)
-      set2
-      (adjoin-set (car set1)(union-set (cdr set1) set2))))
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((= (car set1) (car set2))
+         (cons (car set1)
+               (union-set (cdr set1) (cdr set2))))
+        ((< (car set1) (car set2))
+         (cons (car set1) (union-set (cdr set1) set2)))
+        (else 
+         (cons (car set2)
+               (union-set set1 (cdr set2))))))
